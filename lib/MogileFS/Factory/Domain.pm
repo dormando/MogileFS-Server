ce1eeb7743097193e068b2091a0f79e67ead3821
dormando <dormando@rydia.net>
2011-05-26 14:49:56 -0700

    Restore default class "0"
    Also fix calls from mogfileinfo and mogfiledebug

--- lib/MogileFS/Factory/Domain.pm
+++ lib/MogileFS/Factory/Domain.pm
@@ -8,7 +8,17 @@ use MogileFS::Domain;
 sub set {
     my ($self, $args) = @_;
     my $classfactory = MogileFS::Factory::Class->get_factory;
-    return $self->SUPER::set(MogileFS::Domain->new_from_args($args, $classfactory));
+    my $dom = $self->SUPER::set(MogileFS::Domain->new_from_args($args, $classfactory));
+
+    # Stupid awkward classes have a magic "default"
+    # If it exists in the DB, it will be overridden.
+    my $cls = $classfactory->get_by_id($dom->id, 0);
+    unless ($cls) {
+        $classfactory->set({ dmid => $dom->id, classid => 0,
+            classname => 'default',
+            mindevcount => MogileFS->config('default_mindevcount')});
+    }
+    return $dom;
 }
 
 1;
